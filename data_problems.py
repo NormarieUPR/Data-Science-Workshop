743aa9f78c75a95b8b05efad868816c48aa63a80
NormarieUPR <normarie.martinez@upr.edu>
2026-03-14 12:38:57 -0400

    Add Streamlit app, theme, and students dataset
    Adds a Streamlit tutorial app (data_problems.py) demonstrating CSV import, dataset inspection, filtering by major, and visualizations (scatter and bar charts, custom axis selection). Also includes .streamlit/config.toml to configure the app theme/colors and a sample students.csv containing 50 example student records used by the app.

---
+++ data_problems.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import pandas as pd 
+
+st.write( """
+
+# Data Science WorkShop Part 1
+         
+### ColorStack UPRM
+         
+Feel free to interact with the website and play around with the values of the data provided below!
+         
+#### Resources:
+         
+Some links that might be useful when implementing your own datasets 
+
+- [10 minutes to panda](https://pandas.pydata.org/docs/user_guide/10min.html)
+         
+Most importantly have fun :)
+         
+
+""") 
+
+st.write(""" ## 1. Import the data set(s) 
+         
+Here is an example importing a the *csv* file we are using today: """ )
+
+st.code("df = pd.read_csv('students.csv')", language="python")
+
+
+df = pd.read_csv('students.csv')
+
+
+st.write("Is good practice to use a command such as head() to look at the first few rows to see" \
+" what your data set is about")
+st.write("**Dataset Shape:** (row, columns)", df.shape)
+st.dataframe(df.head())
+st.space(size="small")
+
+st.write("## 2. Filter Your Data ")
+major = st.selectbox("Choose a major", df["major"].unique()) 
+
+filtered = df[df["major"] == major]
+
+st.write(filtered)
+
+st.space(size="small")
+st.write("### GPA vs Study Hours")
+st.write("""Vizualize the relaitionship between GPA and study hours 
+- Does it defer from major to major?
+- Does study hours affect GPA?
+- What type of graph is showing? """)
+
+option = st.selectbox("Filter by major or all", ["All Students", "Filtered by Major"] )
+gpavsstudy = df  
+if option == "All students":
+    gpavsstudy = df
+if option == "Filtered by Major":
+    gpavsstudy = filtered
+st.scatter_chart(gpavsstudy, x="study_hours_per_week", y= "gpa", height="content")
+
+st.write(""" ### Club vs Satisfaction
+        
+Are students happier when they join clubs? """)
+
+clubs = df.groupby("num_clubs")["satisfaction_score"].mean()
+
+st.bar_chart(clubs)
+
+
+st.write(""" ### Make comparisons yourself !
+Ask yourself how different columns relate to each other.
+and if they are relevant for the purpose of your project""")
+
+x = st.selectbox("Choose a x axis", df.columns)
+
+y = st.selectbox("Choose a y axis", df.columns)
+
+st.scatter_chart(df,x=x,y=y)
+
+st.write(" #### Ask yourself:")
+st.write("""  - Do students who study more sleep less?
+
+- Do students with jobs have lower GPA?
+
+- Do students who live far away attend less?
+
+- Do students with clubs have higher satisfaction?""")
